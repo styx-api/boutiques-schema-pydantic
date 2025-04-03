7c719d2f819960a163e5bbbdf8b6f9c527c0fba6
Jason Kai <21226986+kaitj@users.noreply.github.com>
2025-04-03 12:58:32 -0400

    Add pydantic frontend

---
+++ src/boutiques_schema_pydantic/frontend/schema/__init__.py
@@ -0,0 +1,9 @@
+""".. include:: ../../README.md"""  # noqa: D415
+
+from typing import Annotated
+
+from pydantic import StringConstraints
+
+StringProperty = Annotated[str, StringConstraints(min_length=1)]
+
+__all__ = ["StringProperty"]
